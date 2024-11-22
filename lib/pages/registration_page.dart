7d966e03f5e41613ab4b675b4846d78649beee4c
James Jordan Vergara <jjrvergara10@gmail.com>
2024-11-22 17:08:05 +0800

    add validations

--- lib/pages/registration_page.dart
+++ lib/pages/registration_page.dart
@@ -150,6 +150,9 @@ class _RegistrationPageState extends State<RegistrationPage> {
                             if (value == null || value.isEmpty) {
                               return 'Password is required';
                             }
+                            if(value.length < 8) {
+                              return 'Password must be at least 8 characters long';
+                            }
                             return null;
                           },
                         ),
